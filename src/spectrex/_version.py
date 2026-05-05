48a4852855e34892ae99b78e360bc0dd47e4865d
Morgan Fouesneau <fouesneau@mpia.de>
2026-05-05 08:48:21 +0200

    minor

--- src/spectrex/_version.py
+++ src/spectrex/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev0+d20260504'
-__version_tuple__ = version_tuple = (0, 1, 'dev0', 'd20260504')
+__version__ = version = '0.1.dev1+g67f4edcb5.d20260505'
+__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g67f4edcb5.d20260505')
 
 __commit_id__ = commit_id = None
